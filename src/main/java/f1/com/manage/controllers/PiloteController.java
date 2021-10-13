60b0474c5b16beaf1f213a40531bd62782605446
Jexblue <91774746+Jexblue@users.noreply.github.com>
2021-10-13 19:32:43 +0400

    Update PiloteController.java

--- src/main/java/f1/com/manage/controllers/PiloteController.java
+++ src/main/java/f1/com/manage/controllers/PiloteController.java
@@ -1,5 +1,6 @@
 package f1.com.manage.controllers;
 
+import f1.com.manage.models.PiloteModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class PiloteController {
 
     @GetMapping("/pilote")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "pilote";
+    public String getAllPilote(PiloteModel piloteModel){
+        
     }
 
 }
