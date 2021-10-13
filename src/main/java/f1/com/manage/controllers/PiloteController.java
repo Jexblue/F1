2c263e0c89e57cb9492be40a7e1c8e103a755df3
Jexblue <91774746+Jexblue@users.noreply.github.com>
2021-10-13 22:31:53 +0400

    Update PiloteController.java

--- src/main/java/f1/com/manage/controllers/PiloteController.java
+++ src/main/java/f1/com/manage/controllers/PiloteController.java
@@ -1,17 +1,24 @@
 package f1.com.manage.controllers;
 
 import f1.com.manage.models.PiloteModel;
+import f1.com.manage.repositories.PiloteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class PiloteController {
 
+    @Autowired
+    PiloteRepository piloteRepository;
+
     @GetMapping("/pilote")
     public String getAllPilote(PiloteModel piloteModel){
-        
+        List<PiloteModel> pilote = piloteRepository.findAll();
     }
 
 }
