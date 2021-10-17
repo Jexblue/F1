3b4bd128398c05a305d6d123d9f624b72b986df8
Jexblue <91774746+Jexblue@users.noreply.github.com>
2021-10-17 20:47:30 +0400

    Update PiloteController.java

--- src/main/java/f1/com/manage/controllers/PiloteController.java
+++ src/main/java/f1/com/manage/controllers/PiloteController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -16,10 +18,25 @@ public class PiloteController {
     @Autowired
     PiloteRepository piloteRepository;
 
-    @GetMapping("/pilote")
+    /*@GetMapping("/pilote")
     public String getAllPilote(PiloteModel piloteModel){
         List<PiloteModel> pilote = piloteRepository.findAll();
         return "";
+    }*/
+
+    /*@GetMapping("/pilote")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        // model.addAttribute("name", name);
+        List<PiloteModel> pilotes = piloteRepository.findAll();
+
+        model.addAttribute("pilote", pilotes);
+        return "pilote";
+    }*/
+
+    @RequestMapping(value = "pilote", method = RequestMethod.GET)
+    public String messages(Model model){
+        model.addAttribute("pilotes", piloteRepository.findAll());
+        return "pilote/listPilote";
     }
 
 }
