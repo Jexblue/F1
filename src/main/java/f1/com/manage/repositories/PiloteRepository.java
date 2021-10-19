ded7ecbbf6df581a473576276c66a4c25539ff0b
Jexblue <91774746+Jexblue@users.noreply.github.com>
2021-10-19 20:51:30 +0400

    Update PiloteRepository.java

--- src/main/java/f1/com/manage/repositories/PiloteRepository.java
+++ src/main/java/f1/com/manage/repositories/PiloteRepository.java
@@ -2,6 +2,7 @@ package f1.com.manage.repositories;
 
 import f1.com.manage.models.PiloteModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
